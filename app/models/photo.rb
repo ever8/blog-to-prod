8c37ecdba26b7ff0f9a3f1bbc6967cf293d15e59
Michael Shelemba <an.hp.ever@gmail.com>
2017-12-13 22:06:45 +0200

    prj config

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,7 +3,7 @@ class Photo < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_many :votes
-
+  validates :photo, presence: true
   mount_uploader :photo, PhotoUploader
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
 	belongs_to :user
 	has_many :postcomments
 	validates :title,:body, presence: true
-	validates :title, length: { in: 3..10 }
+	validates :title, length: { in: 2..32 }
 end
